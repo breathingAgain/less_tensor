d56baab53d29e748d21c976d443b0004b7948151
Stepan <94863192+breathingAgain@users.noreply.github.com>
2021-11-24 15:21:12 -0500

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,14 @@
+#!/bin/bash
+#task1
+a = float(input('Input a: '))
+b = float(input('Input b: '))
+print('addition result ', a + b)
+print('miltiplication result ', a * b)
+print('subtraction result ', a - b)
+print('division result ', a / b)
+print('exponrntiation result ', pow(a,b))
+print('modul dividion result ', a % b)
+print('integer division result ', a // b)
+#task2
+name = input("What's your name?: ")
+print('Hello,', name, '!!')
