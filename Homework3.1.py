58e2dfb97dead763aa2f9db0c3cdcb2516aa3d1c
Stepan <94863192+breathingAgain@users.noreply.github.com>
2021-11-29 06:55:41 -0500

    Add files via upload

---
+++ Homework3.1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+list = [0,0]
+print('Left/Right/Up/Down?')
+word=input()
+print('How much?')
+value=int(input())
+if word=='Left':
+    list[0]-=value
+    print(list)
+elif word=="Right":
+    list[0]+=value
+    print(list)
+elif word=="Up":
+    list[1]+=value
+    print(list)
+elif word=="Down":
+    list[1]-=value
+    print(list)
