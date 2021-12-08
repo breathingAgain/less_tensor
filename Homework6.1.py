361b3125064fff11ba1da9204fc4e30f4430e482
Stepan <94863192+breathingAgain@users.noreply.github.com>
2021-12-08 03:23:29 -0500

    Add files via upload

---
+++ Homework6.1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+def transform_en(str):
+    for i in range(len(str)):
+        str[i] = str[i].encode('utf-8')
+    return str
+     
+def transform_de(str):
+    for i in range(len(str)):
+        str[i] = str[i].decode('utf-8')
+    return str
+
+str = ['а','б','в']
+b = [b'\xd0\xb0', b'\xd0\xb1', b'\xd0\xb2']
+print('Закодированный список строк' , transform_en(str))
+print('Декодированный список байтов' , transform_de(b))
+
+
---
+++ Homework6.2.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+import os
+
+def moleculs(odds):
+    count = 0
+    while True:
+        if (odds[0] // 2 != 0 and odds[1] // 6 != 0 and odds[2] // 1 != 0):
+           odds[0] -= 2
+           odds[1] -= 6
+           odds[2] -= 1
+           count += 1
+        else:
+             break
+    return str(count)
+
+        
+
+
+file=str(input("Введите название файла: ")) # Input.txt; первая строка - C
+                                            # вторая строка - O, третья строка - H
+if os.path.exists(file):                     
+    f = open(file, 'r')      
+else:
+    print('Нет такого файла')
+    exit(0)
+
+lines = f.readlines()
+f.close
+odds = [int(x) for x in lines]
+f = open('Output.txt','w')
+rec = 'number of molecules: ' + moleculs(odds)
+f.write(rec)
+f.close
+
+
+
+
+
+
