30fd548a09ab76486ccda5da3127b184b856bdee
Stepan <94863192+breathingAgain@users.noreply.github.com>
2021-12-13 15:17:27 +0300

    Update Homework4.1.py

--- Homework4.1.py
+++ Homework4.1.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
-a = [4,6,67,5,89,0,23,12,89,9]
+a = [4, 6, 67, 5, 89, 
+     0, 23, 12, 89, 9]
 print(a)
 for i in range(len(a)-1):
-        for j in range(len(a)-i-1):
-            if a[j] > a[j+1]:
-                buff = a[j]
-                a[j] = a[j+1]
-                a[j+1] = buff
-                
+    for j in range(len(a)-i-1):
+        if a[j] > a[j+1]:
+            buff = a[j]
+            a[j] = a[j+1]
+            a[j+1] = buff
+
 print(a)
