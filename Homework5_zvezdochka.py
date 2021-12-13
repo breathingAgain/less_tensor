7576d8e8c9f15d1266e91ac1e81285637d8d31da
Stepan <94863192+breathingAgain@users.noreply.github.com>
2021-12-13 15:14:18 +0300

    Update Homework5_zvezdochka.py

--- Homework5_zvezdochka.py
+++ Homework5_zvezdochka.py
@@ -1,18 +1,16 @@
-
 def fibonaccinum(a):
     if a == 0 or a == 1:
         return a
-    
+
     return fibonaccinum(a-1) + fibonaccinum(a-2)
 
+
 try:
-   num = int(input())
-   assert num > 0
+    num = int(input())
+    assert num > 0
 except ValueError:
     print('Номер должно быть целочисленный')
 except AssertionError:
     print('Номер отрицательный')
 
 print(fibonaccinum(num))
-
-
