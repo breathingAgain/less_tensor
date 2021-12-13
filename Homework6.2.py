7cf7576f8cf690dbf61075ddabfbd3b13a190a65
Stepan <94863192+breathingAgain@users.noreply.github.com>
2021-12-13 15:13:22 +0300

    Update Homework6.2.py

--- Homework6.2.py
+++ Homework6.2.py
@@ -1,25 +1,24 @@
 #!/usr/bin/env python
 import os
 
+
 def moleculs(odds):
     count = 0
     while True:
         if (odds[0] // 2 != 0 and odds[1] // 6 != 0 and odds[2] // 1 != 0):
-           odds[0] -= 2
-           odds[1] -= 6
-           odds[2] -= 1
-           count += 1
+            odds[0] -= 2
+            odds[1] -= 6
+            odds[2] -= 1
+            count += 1
         else:
-             break
+            break
     return str(count)
 
-        
 
-
-file=str(input("Введите название файла: ")) # Input.txt; первая строка - C
-                                            # вторая строка - O, третья строка - H
-if os.path.exists(file):                     
-    f = open(file, 'r')      
+file = str(input("Введите название файла: "))  # Input.txt; первая строка - C
+# вторая строка - O, третья строка - H
+if os.path.exists(file):
+    f = open(file, 'r')
 else:
     print('Нет такого файла')
     exit(0)
@@ -27,11 +26,10 @@ else:
 lines = f.readlines()
 f.close
 odds = [int(x) for x in lines]
-f = open('Output.txt','w')
+f = open('Output.txt', 'w')
 rec = 'number of molecules: ' + moleculs(odds)
 f.write(rec)
 f.close
-
 
 
 
