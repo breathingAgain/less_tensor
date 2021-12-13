741f56fd7013193e9791a485d99b2f1afb48c7d9
Stepan <94863192+breathingAgain@users.noreply.github.com>
2021-12-13 15:18:09 +0300

    Update Homework3.2.py

--- Homework3.2.py
+++ Homework3.2.py
@@ -1,24 +1,24 @@
 #!/usr/bin/env python
-list = [0,0]
+list = [0, 0]
 while True:
-   print('Left/Right/Up/Down?')
-   word=input()
-   print('How much?')
-   value=int(input())
-   if word=='Left':
-      list[0]-=value
-      print(list)
-   elif word=="Right":
-        list[0]+=value
+    print('Left/Right/Up/Down?')
+    word = input()
+    print('How much?')
+    value = int(input())
+    if word == 'Left':
+        list[0] -= value
         print(list)
-   elif word=="Up":
-        list[1]+=value
+    elif word == "Right":
+        list[0] += value
         print(list)
-   elif word=="Down":
-        list[1]-=value
+    elif word == "Up":
+        list[1] += value
         print(list)
-   
-   print('Stop? (Yes/No)')
-   stop=input()
-   if stop=='Yes':
-       break
+    elif word == "Down":
+        list[1] -= value
+        print(list)
+
+    print('Stop? (Yes/No)')
+    stop = input()
+    if stop == 'Yes':
+        break
