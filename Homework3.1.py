47b49fe019bd76d17f3afe9d084bea607560e92e
Stepan <94863192+breathingAgain@users.noreply.github.com>
2021-12-13 15:18:28 +0300

    Update Homework3.1.py

--- Homework3.1.py
+++ Homework3.1.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python
-list = [0,0]
+list = [0, 0]
 print('Left/Right/Up/Down?')
-word=input()
+word = input()
 print('How much?')
-value=int(input())
-if word=='Left':
-    list[0]-=value
+value = int(input())
+if word == 'Left':
+    list[0] -= value
     print(list)
-elif word=="Right":
-    list[0]+=value
+elif word == "Right":
+    list[0] += value
     print(list)
-elif word=="Up":
-    list[1]+=value
+elif word == "Up":
+    list[1] += value
     print(list)
-elif word=="Down":
-    list[1]-=value
+elif word == "Down":
+    list[1] -= value
     print(list)
